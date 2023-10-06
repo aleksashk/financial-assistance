63aad8a9fd1822648ffb60e6adbcf2012d88b9c6
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-10-06 20:13:54 +0300

    refactoring

--- src/main/java/by/flameksandr/App.java
+++ src/main/java/by/flameksandr/App.java
@@ -7,7 +7,7 @@ import static org.apache.commons.codec.digest.DigestUtils.md5Hex;
 public class App {
     public static void main(String[] args) throws SQLException {
         String email = "user1@example.com";
-        String password = "password";
+        String password = "password1";
         String passwordHex = md5Hex(password);
 
         Connection con = DriverManager.getConnection(
@@ -19,10 +19,11 @@ public class App {
         Statement statement = con.createStatement();
         ResultSet rs = statement.executeQuery("select * from user_service");
         while (rs.next()) {
-//            System.out.println(rs.getLong("id") + ", " +
-//                    rs.getString("email") + ", " +
-//                    rs.getString("password"));
+            System.out.println(rs.getLong("id") + ", " +
+                    rs.getString("email") + ", " +
+                    rs.getString("password"));
         }
+        System.out.println("-------------------------------------------------");
         PreparedStatement ps = con.prepareStatement("select * from user_service where email=? and password=?");
         ps.setString(1, email);
         ps.setString(2, passwordHex);
